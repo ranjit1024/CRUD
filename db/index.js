934a89c84d14ee4700314f6ca1ab53df0e5346aa
ranjit1024 <ranjitdas2048@gmail.com>
2024-09-30 11:46:24 +0530

    modifying things

--- db/index.js
+++ db/index.js
@@ -1,1 +1,25 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb+srv://ranjit:iamranjit@cluster0.ehxzb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+
+const AdminSchema = new mongoose.Schema({
+    
+})
+
+const UserSchema = new mongoose.Schema({
+
+})
+
+const CourseSchema = new mongoose.Schema({
+
+})
+
+const Admin = mongoose.model('Admin', AdminSchema);
+const User = mongoose.model('User', UserSchema);
+const Course = mongoose.model('Course', CourseSchema);
+
+module.exports = {
+    Admin,
+    User,
+    Course
+}
