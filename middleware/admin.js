1e5b3b44d7cd61fc196d3e4daf8ad6ef77be7436
ranjit1024 <ranjitdas2048@gmail.com>
2024-09-30 22:20:49 +0530

    done with db and middleware

--- middleware/admin.js
+++ middleware/admin.js
@@ -1,4 +1,22 @@
+
+const {Admin} = require("../db");
+console.log(Admin);
+
 function adminMiddleWare(req, res, next){
+    const username = req.headers.username;
+    const password = req.headers.password;
+    Admin.findOne({
+        username:username,
+        password:password
+    }).then(function(value){
+        if(value){
+            next();
+        }else{
+            res.status(403).json({
+                msg:"admin does not exists"
+            })
+        }
+    })
 
 }
 module.exports = adminMiddleWare; 
--- middleware/user.js
+++ middleware/user.js
@@ -1,4 +1,21 @@
-function userMidlleware(req, res, next){
+const {User} = require('../db');
 
+function userMidlleware(req, res, next){
+    const username = req.headers.username;
+    const password = req.headers.password;
+
+    User.findOne({
+        username:username,
+        password:password,
+
+    }).then(value => {
+        if(value){
+            next()
+        }else{
+            res.status(403).res.json({
+                msg:"user Not found"
+            })
+        }
+    })
 }
 module.exports = userMidlleware;
