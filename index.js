0a904aaae3bcaf1ba480373d5a0929d4c7488e3e
ranjit1024 <ranjitdas2048@gmail.com>
2024-09-30 10:31:38 +0530

    adding basic configrition

---
+++ index.js
@@ -0,0 +1,5 @@
+// const express = require("express");
+// const bsodyParser = require('body-parser');
+const adminRouter = require("./routes/user.js");
+
+console.log(adminRouter.getThings)
---
+++ routes/user.js
@@ -0,0 +1,5 @@
+function getThings(){
+    console.log("this is woring")
+    return 1;
+}
+module.exprorts =  getThings();
